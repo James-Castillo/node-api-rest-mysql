aaf4b87d83c87fbeb846913ec5481f96c584664d
James Castillo <rubenjamescastillogarcia12@gmail.com>
2024-05-01 20:11:26 -0600

    Proyecto crud productos con node, express, mysql

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,14 +1,18 @@
 import { Sequelize } from 'sequelize';
 
 // Obtener los valores de las variables de entorno o establecer valores predeterminados
-const dbName = process.env.DB_NAME || 'almacen';
-const dbUser = process.env.DB_USER || 'root';
-const dbPassword = process.env.DB_PASSWORD || 'KrJuniors08$';
-const dbHost = process.env.DB_HOST || 'localhost';
+const dbName = process.env.DB_NAME || '';
+const dbUser = process.env.DB_USER || '';
+const dbPassword = process.env.DB_PASSWORD || '';
+const dbHost = process.env.DB_HOST || '';
+
+// Convertir el puerto de cadena a número
+const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
-  dialect: 'mysql'
+  port: dbPort, // Utiliza el puerto convertido a número
+  dialect: 'mysql',
 });
 
 export default sequelize;
